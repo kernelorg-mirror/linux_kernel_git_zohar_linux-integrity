9a188a77a269c868e5f216ccfbf43d5645e92471
Mimi Zohar <zohar@linux.vnet.ibm.com>
2015-12-28 16:02:29 -0500

    vfs: define a generic function to read a file from the kernel
    In order to measure and appraise files being read by the kernel,
    new module and kexec syscalls were defined which include a file
    descriptor.  Other places in the kernel (eg. firmware, IMA,
    sound) also read files.
    
    This patch introduces a common function for reading files from
    the kernel with the corresponding security post-read hook and
    function.
    
    Changelog:
    - Add missing <linux/vmalloc.h>
    
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -561,6 +561,14 @@
  *	the kernel module to load. If the module is being loaded from a blob,
  *	this argument will be NULL.
  *	Return 0 if permission is granted.
+ * @kernel_post_read_file:
+ *	Read a file specified by userspace.
+ *	@file contains the file structure pointing to the file being read
+ *	by the kernel.
+ *	@buf pointer to buffer containing the file contents.
+ *	@size length of the file contents.
+ *	@policy_id contains the calling function identifier.
+ *	Return 0 if permission is granted.
  * @task_fix_setuid:
  *	Update the module's state after setting one or more of the user
  *	identity attributes of the current process.  The @flags parameter
@@ -1457,6 +1465,8 @@ union security_list_options {
 	int (*kernel_fw_from_file)(struct file *file, char *buf, size_t size);
 	int (*kernel_module_request)(char *kmod_name);
 	int (*kernel_module_from_file)(struct file *file);
+	int (*kernel_post_read_file)(struct file *file, char *buf, loff_t size,
+				     int policy_id);
 	int (*task_fix_setuid)(struct cred *new, const struct cred *old,
 				int flags);
 	int (*task_setpgid)(struct task_struct *p, pid_t pgid);
@@ -1716,6 +1726,7 @@ struct security_hook_heads {
 	struct list_head kernel_act_as;
 	struct list_head kernel_create_files_as;
 	struct list_head kernel_fw_from_file;
+	struct list_head kernel_post_read_file;
 	struct list_head kernel_module_request;
 	struct list_head kernel_module_from_file;
 	struct list_head task_fix_setuid;
